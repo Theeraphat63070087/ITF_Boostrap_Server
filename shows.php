95c62894abcbceb83cfd60593c2500b50b0dc00f
Theeraphat63070087 <73682336+Theeraphat63070087@users.noreply.github.com>
2020-11-19 20:55:14 +0700

    Update shows.php

--- shows.php
+++ shows.php
@@ -62,15 +62,15 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 							<div class="col-6 text-right"><a href="form.php" class="btn btn-warning btn-sm font-weight-bold">ADD</a></div>
                         </div>
                         <div class="table-responsive">
-<table width="900" border="1" id="customers">
+<table border="1" id="customers" class='table'>
   <tr>
-    <th width="100"> <div align="center">First Name</div></th>
-    <th width="100"> <div align="center">Last Name </div></th>
-    <th width="100"> <div align="center">Gender </div></th>
-    <th width="100"> <div align="center">Age </div></th>
-    <th width="100"> <div align="center">Phone Number </div></th>
-    <th width="100"> <div align="center">Email </div></th>
-    <th width="100"> <div align="center">Action </div></th>
+    <th> <div align="center">First Name</div></th>
+    <th> <div align="center">Last Name </div></th>
+    <th> <div align="center">Gender </div></th>
+    <th> <div align="center">Age </div></th>
+    <th> <div align="center">Phone Number </div></th>
+    <th> <div align="center">Email </div></th>
+    <th> <div align="center">Action </div></th>
 
   </tr>
 <?php
